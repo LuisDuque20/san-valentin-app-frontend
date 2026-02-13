c6e38bf4842d9132560aa160fde2652313d854ee
LuisDuque20 <00013423@uca.edu.sv>
2026-02-12 19:48:02 -0600

    subiendo correccion

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export const sendConfirmation = async (email) => {
-  const res = await fetch('http://localhost:3000/api/mail/send-confirmation', {
+  const res = await fetch('https://san-valentin-app-backend.onrender.com/api/mail/send-confirmation', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email })
